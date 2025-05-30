83006317c0c045fa1287ea197833a0f56432e1de
HekticXoX <hekticxox@gmail.com>
2025-05-30 03:16:46 -0400

    Bringing up to date.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,6 @@
-// (optional - can be empty)
+// Top-level build file
+
+plugins {
+    id("com.android.application") version "8.10.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+}
